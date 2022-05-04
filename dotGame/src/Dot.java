7228c901f5ad2350cea4fa8214f1610592300d0b
Yuen Law <yuen.lw@gmai.com>
2022-05-04 14:41:12 -0600

    ADD move() de Dot

--- dotGame/src/Dot.java
+++ dotGame/src/Dot.java
@@ -1,3 +1,26 @@
-public class Dot {
+public class Dot implements Constantes{
     
+    int[] target;
+    int[] currentPosition;
+
+    public Dot(){
+        currentPosition = new int[2];
+        currentPosition[X] = 0;
+        currentPosition[Y] = 0;
+
+        target = new int[2];
+        target[X] = 0;
+        target[Y] = 0;
+    }
+
+
+    public void move(){
+        if(target[X] != currentPosition[X]){
+            currentPosition[X] += (target[X]-currentPosition[X])/Math.abs(target[X]-currentPosition[X]);
+        }
+        if(target[Y] != currentPosition[Y]){
+            currentPosition[Y] += (target[Y]-currentPosition[Y])/Math.abs(target[Y]-currentPosition[Y]);
+        }
+    }
+
 }
