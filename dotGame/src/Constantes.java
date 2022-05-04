7abf53a6be39f4ce3f3c50103907df9350dc05a9
Yuen Law <yuen.lw@gmai.com>
2022-05-04 14:26:19 -0600

    FIX constructor casilla

--- dotGame/src/Constantes.java
+++ dotGame/src/Constantes.java
@@ -7,6 +7,8 @@ public interface Constantes {
     public static final int CASILLA_WIDTH = 50;
     public static final int CASILLA_HEIGHT = 50;
 
+    public static final int TABLERO_SIZE = 15;
+
     public static final Color TARGET_COLOR = Color.GREEN;
     public static final Color BG_COLOR = Color.LIGHT_GRAY;
     public static final Color DOT_COLOR = Color.PINK;
--- dotGame/src/Mapa.java
+++ dotGame/src/Mapa.java
@@ -1,3 +1,24 @@
-public class Mapa {
-    
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.event.*;
+
+public class Mapa implements Constantes{
+    public JPanel panelTablero;
+    public Casilla[][] tablero;
+
+
+    public Mapa(ActionListener listener){
+        panelTablero = new JPanel();
+        panelTablero.setLayout(new GridLayout(TABLERO_SIZE, TABLERO_SIZE));
+        for (int i = 0; i < TABLERO_SIZE; i++){
+            for (int j = 0; j < TABLERO_SIZE; j++){
+                tablero[i][j] = new Casilla(i, j);
+                tablero[i][j].addActionListener(listener);
+                panelTablero.add(tablero[i][j]);
+            }
+        }
+    }
+
+
+
 }
