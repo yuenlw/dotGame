ecd56545410721afdc14852db6ee6f4d79b1b62f
Yuen Law <yuen.lw@gmai.com>
2022-05-04 14:15:25 -0600

    ADD métodos de Casilla

--- dotGame/src/Casilla.java
+++ dotGame/src/Casilla.java
@@ -1,18 +1,38 @@
-import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JButton;
 
-public class Casilla extends JButton{
+public class Casilla extends JButton implements Constantes{
 
     private int[] coordenadas;
 
     public Casilla(int posX, int posY){
         coordenadas = new int[2];
-        coordenadas[0] = posX;
-        coordenadas[1] = posY;
+        coordenadas[X] = posX;
+        coordenadas[Y] = posY;
 
-        setBackground(Color.LIGHT_GRAY);
-        setPreferredSize(new Dimension(50, 50));
+        setBackground(BG_COLOR);
+        setPreferredSize(new Dimension(CASILLA_WIDTH, CASILLA_HEIGHT));
     }
+
+    public int[] getCoords(){
+        return coordenadas;
+    }
+
+    public void setAsTarget(){
+        setBackground(TARGET_COLOR);
+    }
+
+    public void clearTarget(){
+        setBackground(BG_COLOR);
+    }
+
+    public void setAsDot(){
+        setBackground(DOT_COLOR);
+    }
+
+    public void clearDot(){
+        setBackground(BG_COLOR);
+    }
+
 }
--- dotGame/src/Constantes.java
+++ dotGame/src/Constantes.java
@@ -1,9 +1,13 @@
+import java.awt.Color;
+
 public interface Constantes {
     public static final int X = 0;
     public static final int Y = 1;
 
     public static final int CASILLA_WIDTH = 50;
     public static final int CASILLA_HEIGHT = 50;
-    
-    
+
+    public static final Color TARGET_COLOR = Color.GREEN;
+    public static final Color BG_COLOR = Color.LIGHT_GRAY;
+    public static final Color DOT_COLOR = Color.PINK;
 }
