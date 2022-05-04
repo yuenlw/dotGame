0adbf70fad18a060ac54d512dfa83681fdda8ce0
Yuen Law <yuen.lw@gmai.com>
2022-05-04 14:08:10 -0600

    ADD interface constantes

---
+++ dotGame/src/Constantes.java
@@ -0,0 +1,9 @@
+public interface Constantes {
+    public static final int X = 0;
+    public static final int Y = 1;
+
+    public static final int CASILLA_WIDTH = 50;
+    public static final int CASILLA_HEIGHT = 50;
+    
+    
+}
