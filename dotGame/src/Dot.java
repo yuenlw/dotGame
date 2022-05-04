a8f722ede398d0b1729dda2861548868c2b8159e
Yuen Law <yuen.lw@gmai.com>
2022-05-04 15:12:49 -0600

    ADD lastPosition to Dot

--- dotGame/src/Dot.java
+++ dotGame/src/Dot.java
@@ -2,11 +2,16 @@ public class Dot implements Constantes{
     
     int[] target;
     int[] currentPosition;
+    int[] lastPosition;
 
     public Dot(){
         currentPosition = new int[2];
         currentPosition[X] = 0;
         currentPosition[Y] = 0;
+
+        lastPosition = new int[2];
+        lastPosition[X] = 0;
+        lastPosition[Y] = 0;
 
         target = new int[2];
         target[X] = 0;
@@ -15,10 +20,13 @@ public class Dot implements Constantes{
 
 
     public void move(){
+    
         if(target[X] != currentPosition[X]){
+            lastPosition[X] = currentPosition[X];
             currentPosition[X] += (target[X]-currentPosition[X])/Math.abs(target[X]-currentPosition[X]);
         }
         if(target[Y] != currentPosition[Y]){
+            lastPosition[Y] = currentPosition[Y];
             currentPosition[Y] += (target[Y]-currentPosition[Y])/Math.abs(target[Y]-currentPosition[Y]);
         }
     }
