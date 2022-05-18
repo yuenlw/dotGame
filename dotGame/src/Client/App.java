9f85244d89ec9053250ef23c4ebc7892311adb41
yuenlw <yuen.lw@gmail.com>
2022-05-18 14:28:03 -0600

    Add Cliente

---
+++ dotGame/src/Client/App.java
@@ -0,0 +1,6 @@
+package Client;
+public class App {
+    public static void main(String[] args) throws Exception {
+        GUI dotGame = new GUI();
+    }
+}
---
+++ dotGame/src/Client/GUI.java
@@ -0,0 +1,84 @@
+package Client;
+import java.awt.event.*;
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+import Common.Casilla;
+import Common.Constantes;
+import Common.Dot;
+import Common.Mapa;
+
+import java.net.*;
+import java.io.*;
+
+public class GUI implements ActionListener, Constantes{
+
+    JFrame ventana;
+    JButton next;
+    Mapa mapa;
+    Dot dot;
+
+    Socket client;
+    ObjectOutputStream output;
+    
+    public GUI(){
+
+        ventana = new JFrame();
+        mapa = new Mapa(this);
+
+
+        ventana.add(mapa.panelTablero);
+
+        next = new JButton("continuar");
+        next.setActionCommand("next");
+
+        ventana.add(next, BorderLayout.SOUTH);
+
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.pack();
+        ventana.setVisible(true);
+
+        dot = new Dot();
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        mapa.tablero[dot.target.coords[X]][dot.target.coords[Y]].clearTarget();
+        ((Casilla)e.getSource()).setAsTarget();
+        dot.target.coords = ((Casilla)e.getSource()).getCoords();
+
+        try {
+            client = new Socket("127.0.0.1", 4444);
+            output = new ObjectOutputStream(client.getOutputStream());
+            output.writeObject(dot.target);
+            output.flush();
+            output.close();
+            client.close();
+        } catch (Exception ex) {
+            //TODO: handle exception
+        }
+    }
+
+    public void moveDot(){
+        mapa.tablero[dot.lastPosition[X]][dot.lastPosition[Y]].clearDot();
+        mapa.tablero[dot.currentPosition[X]][dot.currentPosition[Y]].setAsDot();
+    
+    }
+
+    public void run(){
+        while (true){
+            dot.move();
+            moveDot();
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                //TODO: handle exception
+            }
+        }
+    }
+
+}
