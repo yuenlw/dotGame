0c32b69231601063e9a1ed1bc9edabbae390b01e
yuenlw <yuen.lw@gmail.com>
2022-05-18 14:01:21 -0600

    Add Server.java

---
+++ dotGame/src/Server/Server.java
@@ -0,0 +1,38 @@
+package Server;
+
+import java.net.*;
+import java.io.*;
+import Common.*;
+
+public class Server {
+    ServerSocket server;
+    Socket client;
+    ObjectInputStream input;
+    Dot dot;
+
+    public Server(Dot d){
+        dot = d;
+        try {
+            server = new ServerSocket(4444);
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+        
+    }
+
+    public void run(){
+        try {
+            while(true){
+                client = server.accept();
+                input = new ObjectInputStream(client.getInputStream());
+                dot.target = (Target)input.readObject();
+                input.close();
+                client.close();
+            }
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+    }
+    
+}
